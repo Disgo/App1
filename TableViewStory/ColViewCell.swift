72759179942609449b2a20a6062a86859e9b7b57
Kristof <kristof@Jennifers-MacBook-Pro.local>
2016-03-08 11:07:50 -0700

    Added collection View

---
+++ TableViewStory/ColViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ColViewCell.swift
+//  UI Collection View
+//
+//  Created by Technology, Arts & Media on 2/25/16.
+//  Copyright © 2016 Disgo. All rights reserved.
+//
+
+import UIKit
+
+class ColViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imgcell: UIImageView!
+    
+    @IBOutlet weak var imgLabel: UILabel!
+
+}
---
+++ TableViewStory/collviewcntrl.swift
@@ -0,0 +1,67 @@
+//
+//  collviewcntrl.swift
+//  UI Collection View
+//
+//  Created by Technology, Arts & Media on 2/25/16.
+//  Copyright © 2016 Disgo. All rights reserved.
+//
+
+import UIKit
+
+class collviewcntrl: UICollectionViewController {
+    
+    var tableImages: [String] = ["tahona-icon.png", "tahona-icon.png", "tahona-icon.png", "tahona-icon.png", "tahona-icon.png", "tahona-icon.png", "tahona-icon.png"]
+    
+    var barNames: [String] = ["Tahona", "Press Play", "The Walrus", "Absinthe House", "Biergarten", "Rio", "Downer"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tableImages.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: ColViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! ColViewCell
+        
+        let backgroundImage = UIImage(named: "blued.jpg")
+        let imageView = UIImageView(image: backgroundImage)
+        self.collectionView!.backgroundView = imageView
+        imageView.contentMode = .ScaleAspectFill
+        
+        cell.imgcell.image = UIImage (named: tableImages [indexPath.row])
+        cell.imgLabel.text = barNames [indexPath.row]
+        return cell
+    }
+override     
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("Cell \(indexPath.row) selected")
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "SendDataSegue" {
+            if let destination = segue.destinationViewController as? ProfileViewController {
+                
+                let cell = sender as! ColViewCell
+                    destination.bar = cell.imgLabel.text!
+            }
+        }
+    }
+   
+
+}
